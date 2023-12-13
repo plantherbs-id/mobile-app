e4d4705eb3a38fac681999d36e863ed88877af88
PlantHerbs <plantherbs.official@gmail.com>
2023-12-13 12:17:55 +0700

    Update ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/plantherbs/app/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/plantherbs/app/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.plantsnap.app", appContext.packageName)
+        assertEquals("com.plantherbs.app", appContext.packageName)
     }
 }
