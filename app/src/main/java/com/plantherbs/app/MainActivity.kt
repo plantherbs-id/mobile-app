ab4ef47c487e3aef8b417a9e8f7204f15bbf5292
Izzudin Adian Khusaini <izzudinadian@gmail.com>
2023-12-13 11:05:54 +0700

    first upload

---
+++ app/src/main/java/com/plantherbs/app/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.plantherbs.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.plantherbs.app.mytestapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+    }
+}
