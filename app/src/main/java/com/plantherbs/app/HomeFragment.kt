ae4da9dac7a79f764eee255ce10db477be1c2e5b
RizqiAziz <rizazi1945@gmail.com>
2023-12-16 20:48:32 +0700

    update
    update gradle

--- app/src/main/java/com/plantherbs/app/HomeFragment.kt
+++ app/src/main/java/com/plantherbs/app/HomeFragment.kt
@@ -1,12 +1,11 @@
 package com.plantherbs.app
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.plantherbs.app.mytestapp.R
-
+import androidx.fragment.app.Fragment
+import com.plantherbs.app.mytesapp.R
 class HomeFragment : Fragment() {
 
     override fun onCreateView(
