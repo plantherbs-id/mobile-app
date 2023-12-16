c1e40e993e19a03f62d3d9d96b7819b26d403038
RizqiAziz <rizazi1945@gmail.com>
2023-12-16 23:17:43 +0700

    gradle
    update

--- app/src/main/java/com/plantherbs/app/onboarding/ViewPagerFragment.kt
+++ app/src/main/java/com/plantherbs/app/onboarding/ViewPagerFragment.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.plantherbs.app.mytestapp.R
+import com.plantherbs.app.R
 import com.plantherbs.app.onboarding.screens.FirstScreen
 import com.plantherbs.app.onboarding.screens.SecondScreen
 import com.plantherbs.app.onboarding.screens.ThirdScreen
--- app/src/main/java/com/plantherbs/app/onboarding/screens/FirstScreen.kt
+++ app/src/main/java/com/plantherbs/app/onboarding/screens/FirstScreen.kt
@@ -6,7 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.viewpager2.widget.ViewPager2
-import com.plantherbs.app.mytestapp.R
+import com.plantherbs.app.R
 import kotlinx.android.synthetic.main.fragment_first_screen.view.*
 import kotlinx.android.synthetic.main.fragment_view_pager.*
 
