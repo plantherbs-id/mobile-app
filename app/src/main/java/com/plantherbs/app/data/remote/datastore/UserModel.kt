d2194680bcee8142d0e70c92ea789918685ffc52
Izzudin Adian Khusaini <99781993+izzudin17@users.noreply.github.com>
2023-12-22 13:50:00 +0700

    Delete app directory

--- app/src/main/java/com/plantherbs/app/data/remote/datastore/UserModel.kt
+++
@@ -1,9 +0,0 @@
-package com.plantherbs.app.data.remote.datastore
-
-data class UserModel(
-    val userId: String,
-    val name: String,
-    val email: String,
-    val token: String,
-    val isLogin: Boolean
-)
